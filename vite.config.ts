177224e6aaeec3d5d42ba4bdbbe44333390f7498
bagatawaya <138273936+bagatawaya@users.noreply.github.com>
2025-07-07 08:41:53 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        { src: 'public/_routes.json', dest: '.' }
-      ]
-    })
-  ],
+  plugins: [react()],
   publicDir: 'public',
   build: {
     outDir: 'dist',
-  },
-  base: '/',
+  }
 })
